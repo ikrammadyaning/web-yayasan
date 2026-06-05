2e1cc7c032f9c742c0c28358f28ee4981d1da3d7
Ikram <ikrammadyaning@gmail.com>
2026-06-05 20:21:48 +0700

    auto to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import AppRouter from './router/AppRouter';
+import ScrollToTop from './components/ScrollToTop';
 import './styles/variables.css';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <main>
         <AppRouter />
